efd6ba38f861698677e206e9718b2b2955de132b
franciscojv1996 <franciscovasquez4000@gmail.com>
2024-12-28 03:05:38 -0400

    se mostro la informacion de la liga

--- app/Http/Controllers/SportsController.php
+++ app/Http/Controllers/SportsController.php
@@ -9,12 +9,62 @@ class SportsController extends Controller
 {
     public function index()
     {
-        $apiKey = env('API_KEY');
+        $basketballKeys = [];
+        $soccerKeys = [];
+        $basketballData = [];
+        $soccerKeysData = [];
+
+        $apiKey = "1fe9dc25095e1dcc4f7ee2c331374298";
         $client = new Client();
         $res = $client->get("https://api.the-odds-api.com/v4/sports/?apiKey={$apiKey}");
         $data = json_decode($res->getBody(), true);
-        return response()->json($data);
+
+        if ($data === null) {
+            return response()->json(['error' => 'Error al decodificar la respuesta de la API'], 500);
+        }
+
+
+        $filterData = array_filter($data, function ($sport) {
+            return in_array($sport['group'], ['Basketball', 'Soccer']);
+        });
+        $filterData = array_values($filterData);
+
+        foreach ($data as $item) {
+            if ($item['group'] === 'Basketball') {
+                $basketballKeys[] = $item['key'];
+            } else if ($item['group'] === 'Soccer') {
+                $soccerKeys[] = $item['key'];
+            }
+        }
+
+        foreach ($basketballKeys as $key) {
+            try {
+                $response = $client->get("https://api.the-odds-api.com/v4/sports/{$key}/odds/?apiKey={$apiKey}&regions=us&markets=h2h,spreads,totals");
+                $basketballData[] = json_decode($response->getBody(), true);
+            } catch (\Exception $e) {
+                error_log("Error fetching data for key {$key}: " . $e->getMessage());
+            }
+        }
+
+        foreach ($soccerKeys as $key) {
+            try {
+                $response = $client->get("https://api.the-odds-api.com/v4/sports/{$key}/odds/?apiKey={$apiKey}&regions=us&markets=h2h,spreads,totals");
+                $soccerKeysData[] = json_decode($response->getBody(), true);
+            } catch (\Exception $e) {
+                error_log("Error fetching data for key {$key}: " . $e->getMessage());
+            }
+        }
+
+        return response()->json([
+            'basketballData' => $basketballData,
+            'soccerKeysData' => $soccerKeysData,
+            'basketballKeys' => $basketballKeys,
+            'soccerKeys' => $soccerKeys,
+        ]);
     }
+
+
+
     public function soccer()
     {
         $apiKey = env('API_KEY');
